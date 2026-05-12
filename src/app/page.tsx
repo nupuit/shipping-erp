ac584a6e29f8832af1aea7a92c8c03944075b194
nupuit <nupuit@gmail.com>
2026-05-12 16:29:17 +0600

    Installed supabase package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { supabase } from "..lib/supabase";
+
+import { supabase } from "@/lib/supabase";
 import { useState } from "react";
 
 export default function Home() {
@@ -9,15 +10,40 @@ export default function Home() {
   const [pol, setPol] = useState("");
   const [pod, setPod] = useState("");
 
+  async function saveBooking() {
+    const { error } = await supabase.from("bookings").insert([
+      {
+        booking_no: bookingNo,
+        ref_no: refNo,
+        pol: pol,
+        pod: pod
+      }
+    ]);
+
+    if (error) {
+      console.error(error);
+      alert("Error ❌");
+    } else {
+      alert("Saved ✅");
+
+      setBookingNo("");
+      setRefNo("");
+      setPol("");
+      setPod("");
+    }
+  }
+
   return (
     <div style={{ fontFamily: "Arial" }}>
 
+      {/* HEADER */}
       <div style={{ background: "#8fa9c9", padding: "10px", color: "white" }}>
         <b>SHIPPING ERP</b>
       </div>
 
       <div style={{ display: "flex" }}>
 
+        {/* SIDEBAR */}
         <div style={{
           width: "250px",
           background: "#dbe4f0",
@@ -31,15 +57,37 @@ export default function Home() {
           <p>CRT</p>
         </div>
 
+        {/* MAIN */}
         <div style={{ flex: 1, padding: "20px" }}>
           <h2>Booking</h2>
 
-          <input value={bookingNo} onChange={(e) => setBookingNo(e.target.value)} placeholder="Booking No" /><br /><br />
-          <input value={refNo} onChange={(e) => setRefNo(e.target.value)} placeholder="Reference No" /><br /><br />
-          <input value={pol} onChange={(e) => setPol(e.target.value)} placeholder="POL" /><br /><br />
-          <input value={pod} onChange={(e) => setPod(e.target.value)} placeholder="POD" /><br /><br />
+          <input
+            value={bookingNo}
+            onChange={(e) => setBookingNo(e.target.value)}
+            placeholder="Booking No"
+          /><br /><br />
 
-          <button>Save Booking</button>
+          <input
+            value={refNo}
+            onChange={(e) => setRefNo(e.target.value)}
+            placeholder="Reference No"
+          /><br /><br />
+
+          <input
+            value={pol}
+            onChange={(e) => setPol(e.target.value)}
+            placeholder="POL"
+          /><br /><br />
+
+          <input
+            value={pod}
+            onChange={(e) => setPod(e.target.value)}
+            placeholder="POD"
+          /><br /><br />
+
+          <button onClick={saveBooking}>
+            Save Booking
+          </button>
 
         </div>
 
@@ -47,3 +95,4 @@ export default function Home() {
     </div>
   );
 }
+``
