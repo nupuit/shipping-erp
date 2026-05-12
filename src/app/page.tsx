0ec2bc549ff38137077f3006bedef2a04582c1aa
nupuit <32977371+nupuit@users.noreply.github.com>
2026-05-12 20:47:51 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../lib/supabase";
 import { useState } from "react";
 
 export default function Home() {
@@ -21,11 +19,9 @@ export default function Home() {
     ]);
 
     if (error) {
-      console.error(error);
       alert("Error ❌");
     } else {
       alert("Saved ✅");
-
       setBookingNo("");
       setRefNo("");
       setPol("");
@@ -34,65 +30,15 @@ export default function Home() {
   }
 
   return (
-    <div style={{ fontFamily: "Arial" }}>
+    
+      Booking
 
-      {/* HEADER */}
-      <div style={{ background: "#8fa9c9", padding: "10px", color: "white" }}>
-        <b>SHIPPING ERP</b>
-      </div>
+       setBookingNo(e.target.value)} />
+       setRefNo(e.target.value)} />
+       setPol(e.target.value)} />
+       setPod(e.target.value)} />
 
-      <div style={{ display: "flex" }}>
-
-        {/* SIDEBAR */}
-        <div style={{
-          width: "250px",
-          background: "#dbe4f0",
-          height: "100vh",
-          padding: "10px"
-        }}>
-          <h4>Agent Tasks</h4>
-          <p>Booking</p>
-          <p>B/L</p>
-          <p>Container Activity</p>
-          <p>CRT</p>
-        </div>
-
-        {/* MAIN */}
-        <div style={{ flex: 1, padding: "20px" }}>
-          <h2>Booking</h2>
-
-          <input
-            value={bookingNo}
-            onChange={(e) => setBookingNo(e.target.value)}
-            placeholder="Booking No"
-          /><br /><br />
-
-          <input
-            value={refNo}
-            onChange={(e) => setRefNo(e.target.value)}
-            placeholder="Reference No"
-          /><br /><br />
-
-          <input
-            value={pol}
-            onChange={(e) => setPol(e.target.value)}
-            placeholder="POL"
-          /><br /><br />
-
-          <input
-            value={pod}
-            onChange={(e) => setPod(e.target.value)}
-            placeholder="POD"
-          /><br /><br />
-
-          <button onClick={saveBooking}>
-            Save Booking
-          </button>
-
-        </div>
-
-      </div>
-    </div>
+      Save
+    
   );
 }
-``
